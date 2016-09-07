ad94ad404462a74e094813455269b77227233bd5
duncan <duncan@DESKTOP-M59DNTC>
2016-09-07 13:30:28 +0800

    完成台位的增删查改

---
+++ NFine.Application/MenuService/SeatApp.cs
@@ -0,0 +1,94 @@
+﻿using NFine.Code;
+using NFine.Domain._03_Entity.MenuBiz;
+using NFine.Domain._04_IRepository.MenuBiz;
+using NFine.Repository.MenuBiz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFine.Application.MenuService
+{
+   public class SeatApp
+    {
+        private IT_SEATRepository service = new T_SEATRepository();
+
+        /// <summary>
+        /// 分页按查询出桌子列表
+        /// </summary>
+        /// <param name="pagination"></param>
+        /// <param name="keyword"></param>
+        /// <param name="OrgId"></param>
+        /// <returns></returns>
+        public List<T_SEATEntity> GetList(Pagination pagination, string keyword, int OrgId)
+        {
+            var expression = ExtLinq.True<T_SEATEntity>();
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                expression = expression.And(t => t.SeatNo.Contains(keyword));
+            }
+            expression = expression.And(t => t.OrgID == OrgId);
+            return service.FindList(expression, pagination);
+        }
+
+        /// <summary>
+        /// 按组织机构查询出所有桌子列表
+        /// </summary>
+        /// <param name="pagination"></param>
+        /// <param name="keyword"></param>
+        /// <param name="OrgId"></param>
+        /// <returns></returns>
+        public List<T_SEATEntity> GetList(string keyword, int OrgId)
+        {
+            var expression = ExtLinq.True<T_SEATEntity>();
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                expression = expression.And(t => t.SeatNo.Contains(keyword));
+            }
+            expression = expression.And(t => t.OrgID == OrgId);
+            Pagination pagination = new Pagination();
+            pagination.sidx = "SortCode desc";
+            pagination.sord = "asc";
+            pagination.rows = 100;
+            pagination.page = 1;
+
+            return service.FindList(expression, pagination);
+        }
+
+        /// <summary>
+        /// 查询单个服务员信息
+        /// </summary>
+        /// <param name="keyValue"></param>
+        /// <returns></returns>
+        public T_SEATEntity GetForm(string keyValue)
+        {
+            return service.FindEntity(int.Parse(keyValue));
+        }
+
+        public void DeleteForm(string keyValue)
+        {
+            int deleteOID = int.Parse(keyValue);
+            service.Delete(t => t.OID == deleteOID);
+        }
+
+        public void SubmitForm(T_SEATEntity objT_SEATEntity, string keyValue)
+        {
+            if (!string.IsNullOrEmpty(keyValue))//编辑
+            {
+                T_SEATEntity oldT_SEATEntity = service.FindEntity(int.Parse(keyValue));
+                oldT_SEATEntity.SeatNo = objT_SEATEntity.SeatNo;
+                oldT_SEATEntity.PersonNum = objT_SEATEntity.PersonNum;
+                oldT_SEATEntity.SaatCategory = objT_SEATEntity.SaatCategory;
+                service.Update(oldT_SEATEntity);
+            }
+            else
+            {
+                objT_SEATEntity.Status = "0";
+                objT_SEATEntity.ParentID = 0;
+                objT_SEATEntity.Desc = objT_SEATEntity.SaatCategory;
+                service.Insert(objT_SEATEntity);
+            }
+        }
+    }
+}
--- NFine.Web/Areas/MenuSys/Controllers/SeatController.cs
+++ NFine.Web/Areas/MenuSys/Controllers/SeatController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NFine.Application.MenuService;
+using NFine.Code;
+using NFine.Domain._03_Entity.MenuBiz;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,9 +17,59 @@ namespace NFine.Web.Areas.MenuSys.Controllers
         //
         // GET: /MenuSys/Seat/
 
+        private SeatApp objSeatApp = new SeatApp();
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        /// <summary>
+        /// 获取列表显示在首页
+        /// </summary>
+        /// <param name="pagination"></param>
+        /// <param name="keyword"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [HandlerAjaxOnly]
+        public ActionResult GetGridJson(Pagination pagination, string keyword)
+        {
+            var data = new
+            {
+                rows = objSeatApp.GetList(pagination, keyword, OperatorProvider.Provider.GetCurrent().OrgId),
+                total = pagination.total,
+                page = pagination.page,
+                records = pagination.records
+            };
+            return Content(data.ToJson());
+        }
+
+        [HttpGet]
+        [HandlerAjaxOnly]
+        public ActionResult GetFormJson(string keyValue)
+        {
+            var data = objSeatApp.GetForm(keyValue);
+            return Content(data.ToJson());
+        }
+
+        [HttpPost]
+        [HandlerAuthorize]
+        [HandlerAjaxOnly]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteForm(string keyValue)
+        {
+            objSeatApp.DeleteForm(keyValue);
+            return Success("删除成功。");
+        }
+
+        [HttpPost]
+        [HandlerAjaxOnly]
+        [ValidateAntiForgeryToken]
+        public ActionResult SubmitForm(T_SEATEntity objT_SEATEntity, string keyValue)
+        {
+            objT_SEATEntity.OrgID = OperatorProvider.Provider.GetCurrent().OrgId;
+            objSeatApp.SubmitForm(objT_SEATEntity, keyValue);
+            return Success("操作成功。");
         }
 
     }
